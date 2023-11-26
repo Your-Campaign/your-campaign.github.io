5a0843db652ae17a1b58126b810eaa5fe73d6aa8
Paul Kreißig <paulkreissig@web.de>
2023-11-26 19:06:45 +0100

    Patch

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"version.json": "f2aadd9e98d57bee431f548e290fc69e",
 "index.html": "5677e5a6efa73673782136bdc6a82246",
 "/": "5677e5a6efa73673782136bdc6a82246",
-"main.dart.js": "1fe7bb1a6ec25554094b4d2d81e416ae",
+"main.dart.js": "1e978f9bcc2057fcdd3e1954eb4ab964",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
