3767b015eb56430e26e361dd53348a8e9071ac89
Paul Kreißig <paulkreissig@web.de>
2023-12-08 09:58:46 +0100

    UI done

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"version.json": "f2aadd9e98d57bee431f548e290fc69e",
 "index.html": "5677e5a6efa73673782136bdc6a82246",
 "/": "5677e5a6efa73673782136bdc6a82246",
-"main.dart.js": "1e675bc30c55aa7319f457bee4bbe8d9",
+"main.dart.js": "990c8a1da79bd1220e5c48d846deea24",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
