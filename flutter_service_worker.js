5d5c9e66fc21556abae24b1269f0ef7e050a0be8
Paul Kreißig <paulkreissig@web.de>
2024-02-02 14:22:02 +0100

    Canvaskit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"version.json": "f2aadd9e98d57bee431f548e290fc69e",
 "index.html": "5677e5a6efa73673782136bdc6a82246",
 "/": "5677e5a6efa73673782136bdc6a82246",
-"main.dart.js": "ed31c926b75dc951be55f5b4266c4763",
+"main.dart.js": "ee80279edeb62df6951ad56b63fcb4bc",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
